5f90dc5263f22a0088a453d6d684c078c2b98282
stevegordon <tdtsgordon@gmail.com>
2015-05-19 23:09:35 -0700

    create user model

---
+++ db/migrate/20150520060907_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.integer :age
+      t.string :gender
+      t.integer :experience
+      t.string :name
+      t.string :password_digest
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
